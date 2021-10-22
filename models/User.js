76cc2449b91e222b62e3537bd66338e4a96eaf8c
hyeonseongOH <dhgustjd10@gmail.com>
2021-10-22 22:34:06 +0900

    주석처리-login오류

--- models/User.js
+++ models/User.js
@@ -68,17 +68,17 @@ userSchema.methods.comparePassword = function(plainPassword, cb){
     })
 
 }
-userSchema.methods.generateToken = function(cb){
-    //jsonwebtoken을 이용해서 token 생성하기
-    var user = this;
-    //user._id와 secretToken을 합쳐서 token을 만든다
-    var token = jwt.sign(user._id,toHexString(), 'secretToken')
-    user.token = token;
-    user.save(function(err, user){
-         if(err) return cb(err)
-         cb(null, user)
-    })
-}
+// userSchema.methods.generateToken = function(cb){
+//     //jsonwebtoken을 이용해서 token 생성하기
+//     var user = this;
+//     //user._id와 secretToken을 합쳐서 token을 만든다
+//     var token = jwt.sign(user._id,toHexString(), 'secretToken')
+//     user.token = token;
+//     user.save(function(err, user){
+//          if(err) return cb(err)
+//          cb(null, user)
+//     })
+// }
 
 const User = mongoose.model('User', userSchema)
 
